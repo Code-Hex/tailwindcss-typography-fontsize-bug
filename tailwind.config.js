4a41ae32ef5f48ffe014f3c68a8dd4a7a6af7408
Brad Cornes <bradlc41@gmail.com>
2022-02-23 12:46:16 +0000

    Fix `h1` styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         DEFAULT: {
           css: {
             h1: {
-              fontSize: theme("fontSize.3xl"), // not reflected.
+              fontSize: theme("fontSize.3xl")[0],
+              ...theme("fontSize.3xl")[1],
               // fontSize: "32px", // reflected
               marginTop: theme("margin.4"), // reflected
             },
